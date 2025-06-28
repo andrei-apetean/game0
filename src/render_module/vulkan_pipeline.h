77828826a5a0f98c6aa537bcdd89ccdfafd0a241
Andrei Apetean <andrei.apetean@gmail.com>
2025-06-28 21:57:20 +0300

    remove old code

--- src/render_module/vulkan_pipeline.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "render_module/vulkan_types.h"
-
-int32_t vulkan_pipeline_create(vulkan_device* device, vulkan_pipeline* pipeline,
-                               vulkan_pipeline_config* config);
-void    vulkan_pipeline_destroy(vulkan_device* device, vulkan_pipeline* pipeline);
